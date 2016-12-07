ec7d72dc1e6fa0d8a969c7c0ef67758ae62d53e0
esix <esix@live.ru>
2016-12-07 15:35:08 +0300

    bixel library

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
-# bixel
+# bixel - BI external element
+
+
+## Usage
+
+1. Download `bixel.js` or install with bower
+- Create html file (index.html)
+- Attach bixel.js as script or through require.js
+- add event handlers (`loading`, `load`, `no-data`, etc)
+- execute ```bixel.init()``` with options described below
+
+## Direct
+- include ```<script src="https://rawgit.com/rcslabs/bixel/master/bixel.js"></script>``` in your html file
+
+## Using Bower
+- run ```bower install git://github.com/rcslabs/bixel```
+- include ```<script src="bower_components/bixel/bixel.js"></script>``` in your html file
+
+## Using require.js
+TODO
+
+# API
+## bixel.init
+
+```
+bixel.init({optional config})
+```
+
+Must execute this function when frame is ready to receive message
+
+Config (optional):
+- locationsCount - number of requested locations
+- metricsCount - number of requested metrics
+- periodsCount - number of requested periods
+
+If none expliced then all selected will be provided
+
+`bixel.init` returns promise object to ensure that everything is working
+```javascript
+bixel.init({periodsCount:1})
+    .then(function() {
+      // succeeded
+    }, function() {
+      // error
+    });
+```
+
+## bixel.on
+subscribe on event
+```
+bixel.on(eventName, callbackFunction)
+```
+eventName is string and can be one of:
+- `loading` - will receive this event when data is loading. A callback will
+  have `Axis` object as the only argument
+- `load` - will receive this event when data is loading. A callback will
+  have `Data` and `Axis` object as two arguments
+- `no-data`- will receive this event when the data is empty if either there is
+   no selection in ui panes or no data on server side. A callback will have
+   `Axis` object as the only argument
+
+## Axis object
+provides methods:
+- `axis.getMetrics()` - returns javascript array of `Metric` objects. This array
+  will be maximum of `metricsCount` length if specified in `bixel.init` method
+- `axis.getLocations()` - returns javascript array of `Location` objects. This array
+  will be maximum of `locationsCount` length if specified in `bixel.init` method
+- `axis.getPeriods()` - returns javascript array of `Period` objects. This array
+  will be maximum of `periodsCount` length if specified in `bixel.init` method
+- `axis.getDimensions()` returns javascript array of `Dimension` objects. It will
+  contain all the dimensions linked with metrics
+
+## Data object
+provides methods:
+- `data.getValue(z, y, x)` - returns `DataItem` object. Arguments `z`, `y` and
+   `x` are  `Metric`, `Location` and `Period`  objects from Axis in any order.
+
+## DataItem
+Generally a javascript `Number` object with overridden `toString` method.
+`toString` will return formatted value according to dimension.
+
+example:
+```javascript
+bixel.on('load', function(data, axis) {
+  var metric = axis.getMetrics()[0];     // only the first metric of provided
+  var location = axis.getLocations()[0]; // and first location
+  var period = axis.getPeriods()[0];     // and first period
+
+  var dataValue = data.getValue(metric, location, period);
+
+  var numValue = dataValue.valueOf();    // ex: number, 1000000
+  var strValue = dataValue.toString()    // ex: string, '$ 1 000 000 us dollars'
+});
+```
+
+
+## Metric object
+javascript object, has fields:
+- `id`:string - unique id of metric
+- `title`:string - title of metric
+- `color`:string - color for this metric according to dash configuration
+- `dim_id`: string - the id of dimension linked to the metric
+
+## Location object
+javascript object, has fields:
+- `id`:string - unique id of location
+- `title`:string - title of location
+- `color`:string - color for this location according to dash configuration
+
+## Period object
+javascript object, has fields:
+- `id`:string - unique id of period
+- `title`:string - title of period
+- `color`:string - color for this period according to dash configuration
---
+++ bixel.js
@@ -0,0 +1,384 @@
+;
+(function (name, definition) {
+	var theModule = definition();
+
+	if (typeof define === 'function' && define.amd) {    // AMD
+		define(theModule);
+	} else if (typeof module !== 'undefined' && module.exports) {  // CommonJS
+		module.exports = theModule
+	} else if (typeof window !== 'undefined') {
+		window[name] = theModule;
+	}
+
+})('bixel', function (undefined) {
+	"use strict";
+
+	var _msg_counter = 0;           // guid counter
+	var _events = {};               // user events by type
+	var _server = new Server();
+
+	var Utils = {
+		genUid: function () {
+			return String(_msg_counter++);
+		}
+	};
+
+
+	/**
+	 * @constructor
+	 * @struct
+	 */
+	function Server() {
+		/**
+		 * Message listeners grouped by type
+		 * @type {{string, Function[]}}
+		 */
+		var _listeners = {};
+
+		/**
+		 * Listener to call when received ACK from server (fullfill and error)
+		 * grouped by message uid
+		 * @type {{string, [Function,Function}}
+		 */
+		var _replyListeners = {};
+
+
+		if ((window !== undefined) && (window.parent !== window)) {
+			if (window.addEventListener) {
+				window.addEventListener('message', _onCrossFrameMessage);
+			} else {
+				window.attachEvent('onmessage', _onCrossFrameMessage);
+			}
+		}
+
+		function _onCrossFrameMessage(event) {
+			try {
+				var srcWindow = event.source;
+				if (srcWindow !== window.parent) {
+					return;                           // accept only messages from parent window
+				}
+				_onMessage(event.data);
+
+			} catch (err) {
+				// ignore
+			}
+		}
+
+		function _crossFrameSend(strMsg) {
+			window.parent.postMessage(strMsg, '*');
+		}
+
+		function _send(msg) {
+			var strMsg = JSON.stringify(msg);
+			_crossFrameSend(strMsg);
+		}
+
+		/**
+		 * Parse message and run handlers
+		 * @param {string} msgStr JSON serialized message
+		 */
+		function _onMessage(msgStr) {
+			try {
+				var msg = JSON.parse(msgStr);
+				if (!('type' in msg)) {
+					return;
+				}
+				var type = msg.type;
+				var uid = msg.uid;
+				var payload = msg.payload;
+
+				var isResponseOk = !!type.match(/^.*_OK$/);
+				var isResponseFailed = !!type.match(/^.*_FAILED$/);
+
+				if (isResponseOk || isResponseFailed) {                   // got response
+					if (uid in _replyListeners) {
+						try {
+							var listenersPair = _replyListeners[uid];
+							var listener = (isResponseOk) ? listenersPair[0] : listenersPair[1];
+							if (typeof listener === 'function') {
+								listener(payload);
+							}
+						} catch (err) {
+							console.log(err.stack);
+						}
+						delete _replyListeners[uid];
+					}
+
+				} else {                                                  // got command
+					var succeeded = false;
+					if (type in _listeners) {
+						_listeners[type].forEach(function (callback) {            // notify all command listeners
+							try {
+								msg.payload = callback(payload);
+								succeeded = true;                                     // if at least one handler is ok: send _OK
+							} catch (err) {
+								if (!succeeded) {
+									msg.payload = {
+										message: err.message
+									};
+								}
+							}
+						});
+					} else {
+						msg.payload = {
+							message: 'no handler'
+						};
+					}
+					msg.type += succeeded ? '_OK' : '_FAILED';
+					_send(msg);                                                 // send response
+				}
+
+			} catch (err) {
+				console.log(err.stack);
+			}
+		}
+
+		/**
+		 * saves message listener
+		 * @param {string} type Message type on which subscribed to
+		 * @param {Function} listener A handler to call when message is received
+		 */
+		this.addMessageListener = function (type, listener) {
+			if (!_listeners.hasOwnProperty(type)) {
+				_listeners[type] = [];
+			}
+			_listeners[type].push(listener);
+		};
+
+		/**
+		 * send message
+		 * @param {string} type Message type
+		 * @param {Object?} payload data to send
+		 */
+		this.send = function (type, payload) {
+			return new Promise(function (fulfill, reject) {
+				var uid = Utils.genUid();
+				var msg = {
+					type: type,
+					payload: payload,
+					uid: uid
+				};
+				_replyListeners[uid] = [fulfill, reject];
+				_send(msg);
+			});
+		}
+	}
+
+	//
+	// register handlers
+	//
+	_server.addMessageListener('LOAD', function (payload) {
+		_onLoad(payload.data, payload.axis); // TODO: + norms
+	});
+	_server.addMessageListener('LOADING', function (payload) {
+		_onLoading(payload.axis);
+	});
+	_server.addMessageListener('NO_DATA', function (payload) {
+		_onNoData(payload.axis);
+	});
+
+
+	function _notifyClient(evtName) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		var evtList = _events[evtName];
+		if (evtList) {
+			for (var i = 0; i < evtList.length; i++) {
+				try {
+					evtList[i].apply(window, args);
+				} catch (err) {
+					// ???
+					console.log(err.stack);
+				}
+			}
+		}
+	}
+
+
+	function formatNum(value, precision) {
+		if (undefined === null || value === null || value === '' || !isFinite(value)) return '';
+		if (precision === undefined) precision = 2;
+		var separator = ' ';
+		if (precision != -1) {
+			value = value.toFixed(precision);
+		}
+		var a = ('' + value).split('.');
+		a[0] = a[0]
+				.split('').reverse().join('')
+				.replace(/\d{3}(?=\d)/g, '$&' + separator)
+				.split('').reverse().join('');
+		if (a.length == 1) {
+			return a[0];
+		} else {
+			return (0 == parseInt(a[1], 10) ? a[0] : a.join('.'));
+		}
+	}
+
+
+	function makeValue(v, dim, digits) {
+		if (v == null) return '-';
+		if (typeof v === 'string') return v;
+		if (v instanceof String) return v.valueOf();
+		if (v instanceof Number) v = v.valueOf();
+		if (isNaN(v)) return '-';
+		if (digits == null) digits = 2;
+		var strValue = (dim && dim.config && dim.config.valueMap && (v in dim.config.valueMap))
+				? dim.config.valueMap[v]
+				: formatNum(v, digits);
+		if (dim) {
+			if (dim.value_prefix) strValue = dim.value_prefix + ' ' + strValue;
+			if (dim.value_suffix) strValue = strValue + ' ' + dim.value_suffix;
+		}
+		return strValue;
+	}
+
+
+	function _createAxis(axis) {
+		var metrics   = axis.metrics;
+		var locations = axis.locations;
+		var periods   = axis.periods;
+		var dims      = axis.dimensions;
+
+		var dh = {}, mh = {}, lh = {}, ph = {};
+		metrics  .forEach(function (m) { mh[m.id] = m });
+		locations.forEach(function (l) { lh[l.id] = l });
+		periods  .forEach(function (p) { ph[p.id] = p });
+		dims     .forEach(function (d) { dh[d.id] = d });
+
+		var result = {};
+
+		result.getMetrics    = function ()   { return metrics;        };
+		result.getLocations  = function ()   { return locations;      };
+		result.getPeriods    = function ()   { return periods;        };
+		result.getDimensions = function ()   { return dims;           };
+
+		result.getMetric     = function (id) { return mh[id] || null; };
+		result.getLocation   = function (id) { return lh[id] || null; };
+		result.getPeriod     = function (id) { return ph[id] || null; };
+		result.getDimension  = function (id) { return dh[id] || null; };
+
+		result.getDimensionByMetric = function (m) { return result.getDimension(m.dim_id) };
+
+		return result;
+	}
+
+
+	var _nalToString = function () {
+		var val = this.valueOf();
+		if (isNaN(val)) {
+			return '-';
+		}
+		return makeValue(val, this.d);    // d - dimension
+	};
+
+
+	//
+	// server event listeners
+	//
+	function _onLoad(data, axis) {
+		axis = _createAxis(axis);
+		var ms = axis.getMetrics();
+		var ls = axis.getLocations();
+		var ps = axis.getPeriods();
+
+		function _createNAL(v, m, l, p) {
+			var nal = (typeof v === "string") ? new String(v) : new Number(v !== null ? v : NaN);
+			nal.m = m;
+			nal.l = l;
+			nal.p = p;
+			nal.d = axis.getDimensionByMetric(m);
+			nal.toString = _nalToString;
+			return nal;
+		}
+
+		var mlpHash = {};
+		data.forEach(function (dataItem) {
+			var h = mlpHash, mid = dataItem.metric_id, lid = dataItem.loc_id, pid = dataItem.period_id;
+			h = (mid in h) ? h[mid] : (h[mid] = {});
+			h = (lid in h) ? h[lid] : (h[lid] = {});
+			h[pid] = _createNAL(dataItem.value, axis.getMetric(mid),  axis.getLocation(lid), axis.getPeriod(pid));
+		});
+
+		var _getItemByMLP = function (m, l, p) {
+			var h = mlpHash;
+			h = (m.id in h) ? h[m.id] : {};
+			h = (l.id in h) ? h[l.id] : {};
+			h = (p.id in h) ? h[p.id] : null;
+			return h;
+		};
+
+		var mlpCube = ms.map(function (m) {
+			return ls.map(function (l) {
+				return ps.map(function (p) {
+					var nal = _getItemByMLP(m, l, p) ;
+					if (nal === null) {
+						nal = _createNAL(null, m, l, p);
+					}
+					return nal;
+				});
+			});
+		});
+
+		var _isM = function (e) {	return ms.indexOf(e) !== -1	};
+		var _isL = function (e) {	return ls.indexOf(e) !== -1	};
+		var _isP = function (e) {	return ps.indexOf(e) !== -1	};
+
+		var _findE = function (isE, z, y, x) { return isE(z) && z || isE(y) && y || isE(x) && x || null };
+
+		var _findM = function (z, y, x) { return _findE(_isM, z, y, x) };
+		var _findL = function (z, y, x) { return _findE(_isL, z, y, x) };
+		var _findP = function (z, y, x) { return _findE(_isP, z, y, x) };
+
+		// TODO: skip some values
+		mlpCube.getValue = function (z, y, x) {
+			var m = _findM(z, y, x);
+			var l = _findL(z, y, x);
+			var p = _findP(z, y, x);
+
+			if (!m || !l || !p) {
+				throw 'Unknown axis';
+			}
+
+			var mi = ms.indexOf(m);
+			var li = ls.indexOf(l);
+			var pi = ps.indexOf(p);
+
+			if (mi === -1 || li == -1 || pi == -1) {
+				throw 'Unknown axis';
+			}
+
+			return mlpCube[mi][li][pi];
+		};
+
+		_notifyClient('load', mlpCube, axis);
+	}
+
+	function _onLoading(axis) {
+		_notifyClient('loading', _createAxis(axis));
+	}
+
+	function _onNoData(axis) {
+		_notifyClient('no-data', _createAxis(axis));
+	}
+
+
+	//
+	// bixel object
+	//
+
+	var bixel = {
+		invoke: function(messageType, opt) {
+			return _server.send(messageType, opt);
+		},
+		on: function on(evtType, fn) {
+			var evtList = _events[evtType] || (_events[evtType] = []);
+			evtList.push(fn);
+			return bixel;
+		},
+		// helpers
+		init: function init(opt) {
+			return this.invoke('INIT', opt);
+		}
+	};
+
+	return bixel;
+});
